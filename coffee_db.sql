d9d6ddcb1df78a34b77ea5dc518434944601c4a1
ptmsk <huy.tranacc13042@hcmut.edu.vn>
2022-06-08 02:13:28 +0700

    updated database

--- coffee_db.sql
+++ coffee_db.sql
@@ -2,26 +2,11 @@ drop database if exists coffee_db;
 create database coffee_db;
 USE coffee_db;
 
-# CREATE TABLE
-DROP TABLE IF EXISTS ADDRESS;
-CREATE TABLE ADDRESS (
-	add_num		INT			PRIMARY KEY,
-    street_num	VARCHAR(10),
-    street_name	VARCHAR(20),
-    ward		VARCHAR(20),
-    district	VARCHAR(20),
-    province	VARCHAR(20)
-);
-
 DROP TABLE IF EXISTS BRANCH;
 CREATE TABLE BRANCH (
-	br_id		INT 		PRIMARY KEY,
-    mng_id		INT			NOT NULL,
-    add_num		INT			NOT NULL,
-    CONSTRAINT 	fk_branch_address
-				FOREIGN KEY (add_num) 
-				REFERENCES ADDRESS(add_num)
-                ON DELETE RESTRICT ON UPDATE CASCADE
+	br_id		INT 			PRIMARY KEY,
+    mng_id		INT				NOT NULL,
+    address		VARCHAR(100)	NOT NULL	DEFAULT ''
 );
 
 DROP TABLE IF EXISTS FURNITURE;
@@ -44,7 +29,7 @@ CREATE TABLE EMPLOYEE (
     emp_id		INT			PRIMARY KEY,
     br_id		INT			NOT NULL,
     bdate		DATE		NOT NULL,
-    add_num		INT			NOT NULL,
+    address		VARCHAR(100)NOT NULL	DEFAULT '',
     sex			CHAR(1)		NOT NULL,
     startdate	DATE		NOT NULL,
     ssn			CHAR(10)	NOT NULL,
@@ -58,10 +43,6 @@ CREATE TABLE EMPLOYEE (
     CONSTRAINT 	fk_emp_branch
 				FOREIGN KEY (br_id) 
 				REFERENCES BRANCH(br_id) 
-				ON DELETE RESTRICT ON UPDATE CASCADE,
-	CONSTRAINT 	fk_emp_address
-				FOREIGN KEY (add_num) 
-				REFERENCES ADDRESS(add_num) 
 				ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
@@ -116,8 +97,7 @@ CREATE TABLE PR_PRICE (
 DROP TABLE IF EXISTS MATERIAL;
 CREATE TABLE MATERIAL (
 	m_id		INT			PRIMARY KEY,
-    m_name		VARCHAR(20)	NOT NULL,
-    quantity	FLOAT		NOT NULL
+    m_name		VARCHAR(20)	NOT NULL
 );
 
 DROP TABLE IF EXISTS M_BATCH;
@@ -160,13 +140,10 @@ CREATE TABLE CUSTOMER (
     cus_id		INT			PRIMARY KEY,
     phone_num	CHAR(10)	NOT NULL,
     sex			CHAR(1)		NOT NULL,
-    add_num		INT			NOT NULL,
+    address		VARCHAR(100)NOT NULL	DEFAULT '',
     gmail		VARCHAR(40) NOT NULL,
     res_date	DATE		NOT NULL,
-    CONSTRAINT 	fk_cus_address
-				FOREIGN KEY (add_num) 
-				REFERENCES ADDRESS(add_num) 
-				ON DELETE RESTRICT ON UPDATE CASCADE
+    acc_point	INT			NOT NULL 	DEFAULT 0
 );
 
 DROP TABLE IF EXISTS PROMOTION;
@@ -229,23 +206,19 @@ CREATE TABLE PRODUCT_GIFT (		# san pham duoc dung de tang
 );
 
 DROP TABLE IF EXISTS PR_ORDER;
-CREATE TABLE PR_ORDER (				# don hang
+CREATE TABLE PR_ORDER (	# don hang
 	order_id	INT 	PRIMARY KEY,
     order_date	DATE	NOT NULL,
     order_time	TIME	NOT NULL,
-    promo_red	INT,				# khuyen mai quy doi
-    total		INT		NOT NULL,
+    promo_red	INT					DEFAULT 0,	# khuyen mai quy doi
+    total		INT		NOT NULL	DEFAULT 0,
     order_type	BOOL 	NOT NULL,	# 0: offline, 1: online
-    rec_add		INT,				# noi nhan hang
-    promo_id	INT,
+    rec_address	VARCHAR(100)		DEFAULT '',		# noi nhan hang
+    promo_id	INT					DEFAULT 0,
     br_id		INT		NOT NULL,
     cus_id		INT		NOT NULL,
     emp_id		INT		NOT NULL,
     stat		BOOL	NOT NULL,	# 0: chua thanh toan, 1: da thanh toan
-    CONSTRAINT 	fk_receive_address
-				FOREIGN KEY (rec_add) 
-				REFERENCES ADDRESS(add_num) 
-				ON DELETE RESTRICT ON UPDATE CASCADE,
 	CONSTRAINT 	fk_promotion_order
 				FOREIGN KEY (promo_id) 
 				REFERENCES PROMOTION(promo_id) 
@@ -270,7 +243,7 @@ CREATE TABLE RECEIPT (
     order_id	INT		NOT NULL,
     pay_day		DATE	NOT NULL,
     pay_time	TIME	NOT NULL,
-    pay_med		CHAR(1)	NOT NULL,	# phuong thuc thanh toan: tien mat, momo, chuyen khoan
+    pay_med		CHAR(1)	NOT NULL	DEFAULT 'M',	# phuong thuc thanh toan: tien mat, momo, chuyen khoan
     promo_red	INT, 	# khuyen mai quy doi
     br_id		INT		NOT NULL,
     cus_id		INT		NOT NULL,
@@ -282,7 +255,29 @@ CREATE TABLE RECEIPT (
 	CONSTRAINT 	fk_customer_receipt
 				FOREIGN KEY (cus_id) 
 				REFERENCES CUSTOMER(cus_id) 
+				ON DELETE RESTRICT ON UPDATE CASCADE,
+	CONSTRAINT 	fk_order_receipt
+				FOREIGN KEY (order_id) 
+				REFERENCES PR_ORDER(order_id) 
 				ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+DROP TABLE IF EXISTS PRODUCT_ORDER;
+CREATE TABLE  PRODUCT_ORDER (
+	pr_id		INT 	NOT NULL,
+    order_id	INT		NOT NULL,
+    PRIMARY KEY (pr_id, order_id),
+    size		CHAR(1)	NOT NULL,
+    price		INT		NOT NULL,
+    quantity 	INT		NOT NULL,
+    CONSTRAINT	fk_product_order
+				FOREIGN KEY (pr_id)
+                REFERENCES PRODUCT(pr_id)
+                ON DELETE RESTRICT ON UPDATE CASCADE,
+	CONSTRAINT	fk_order_product
+				FOREIGN KEY (pr_id)
+                REFERENCES PR_ORDER(order_id)
+                ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 DROP TABLE IF EXISTS DELI_SERVICE;
@@ -311,3 +306,234 @@ CREATE TABLE DELIVERY (
 				ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
+# insert new order
+DROP PROCEDURE IF EXISTS proc_insert_order;
+DELIMITER $$
+CREATE PROCEDURE proc_insert_order (
+	order_id	INT,
+    order_date	DATE,
+    order_time	TIME,
+    promo_red	INT,	# khuyen mai quy doi
+    total		INT,
+    order_type	BOOL,	# 0: offline, 1: online
+    rec_address	VARCHAR(100),		# noi nhan hang
+    promo_id	INT,
+    br_id		INT,
+    cus_id		INT,
+    emp_id		INT,
+    stat		BOOL	# 0: chua thanh toan, 1: da thanh toan
+) 
+BEGIN
+	IF (promo_red <> 0 AND promo_id <> 0) THEN
+		SIGNAL SQLSTATE '01000'
+			SET MESSAGE_TEXT = 'Only apply one type of promotion at a time!';
+    END IF;
+	INSERT INTO PR_ORDER VALUES(order_id, order_date, order_time, promo_red, total, order_type,
+								red_address, promo_id, br_id, cus_id, emp_id, stat);
+END $$
+DELIMITER ;
+
+
+# insert product to an order
+DROP PROCEDURE IF EXISTS proc_insert_prod_order;
+DELIMITER $$
+CREATE PROCEDURE proc_insert_prod_order (
+	mod_order_id	INT,
+    prod_id			INT,
+    quantity		INT
+)
+BEGIN
+	# SOMETHING HERE
+END $$
+DELIMITER ;
+
+
+# change receive address 
+DROP PROCEDURE IF EXISTS proc_update_receive_address;
+DELIMITER $$
+CREATE PROCEDURE proc_update_receive_address (
+	mod_order_id	INT,
+    mod_address		VARCHAR(100)
+)
+BEGIN
+	IF (SELECT order_id FROM PR_ORDER WHERE PR_ORDER.order_id = mod_order_id) = NULL THEN
+		SIGNAL SQLSTATE '01000'
+			SET MESSAGE_TEXT = 'Cannot find existing order!';
+	END IF;
+    UPDATE PR_ORDER
+    SET PR_ORDER.rec_address = mod_address
+    WHERE PR_ORDER.order_id = mod_order_id; 
+END $$
+DELIMITER ;
+
+
+# update customer id in order
+DROP PROCEDURE IF EXISTS proc_update_cus_id;
+DELIMITER $$
+CREATE PROCEDURE proc_update_cus_id (
+	mod_order_id	INT,
+    mod_cus_id		INT
+)
+BEGIN
+	IF (SELECT order_id FROM PR_ORDER WHERE PR_ORDER.order_id = mod_order_id) = NULL THEN
+		SIGNAL SQLSTATE '01000'
+			SET MESSAGE_TEXT = 'Cannot find existing order!';
+	END IF;
+	UPDATE PR_ORDER
+    SET PR_ORDER.cus_id = mod_cus_id
+    WHERE PR_ORDER.order_id = mod_order_id;
+END $$
+DELIMITER ;
+
+
+# change status flag (false -> true: paid)
+DROP PROCEDURE IF EXISTS proc_set_order_status;
+DELIMITER $$
+CREATE PROCEDURE proc_set_order_status(
+	mod_order_id 	INT
+)
+BEGIN
+	IF (SELECT order_id FROM PR_ORDER WHERE PR_ORDER.order_id = mod_order_id) = NULL THEN
+		SIGNAL SQLSTATE '01000'
+			SET MESSAGE_TEXT = 'Cannot find existing order!';
+	END IF;
+    UPDATE PR_ORDER
+    SET PR_ORDER.stat = TRUE
+    WHERE PR_ORDER.order_id = mod_order_id;
+END $$
+DELIMITER ;
+
+
+# modify promotion
+DROP PROCEDURE IF EXISTS proc_modify_order_promo_id; 
+DELIMITER $$
+CREATE PROCEDURE proc_modify_order_promo_id (
+	mod_order_id 	INT,
+	new_promo_id 	INT
+)
+BEGIN
+	DECLARE promo_red INT DEFAULT 0;
+	IF (SELECT order_id FROM PR_ORDER WHERE PR_ORDER.order_id = mod_order_id) = NULL THEN
+		SIGNAL SQLSTATE '01000'
+			SET MESSAGE_TEXT = 'Cannot find existing order!';
+	END IF;
+    SET promo_red = (SELECT promo_red FROM PR_ORDER WHERE PR_ORDER.order_id = mod_order_id);
+    IF promo_red <> 0 AND new_promo_id <> 0 THEN
+		SIGNAL SQLSTATE '01000'
+			SET MESSAGE_TEXT = 'Only apply one type of promotion at a time!';
+	END IF;
+    UPDATE PR_ORDER 
+    SET PR_ORDER.promo_id = new_promo_id
+    WHERE PR_ORDER.order_id = mod_order_id;
+    # NEED TRIGGER
+END $$
+DELIMITER ;
+
+
+# modify promotion redemption 
+DROP PROCEDURE IF EXISTS proc_modify_order_promo_red;
+DELIMITER $$
+CREATE PROCEDURE proc_modify_order_promo_red (
+	mod_order_id	INT,
+    redem_point		INT
+)
+BEGIN
+	DECLARE total_money INT DEFAULT 0;
+    DECLARE limit_point INT DEFAULT 0;
+    DECLARE promo_id 	INT	DEFAULT 0;
+	IF (SELECT order_id FROM PR_ORDER WHERE PR_ORDER.order_id = mod_order_id) = NULL THEN
+		SIGNAL SQLSTATE '01000'
+			SET MESSAGE_TEXT = 'Cannot find existing order!';
+	END IF;
+    SET promo_id = (SELECT order_id FROM PR_ORDER WHERE PR_ORDER.order_id = mod_order_id);
+    IF promo_id <> 0 AND redem_point <> 0 THEN
+		SIGNAL SQLSTATE '01000'
+			SET MESSAGE_TEXT = 'Only apply one type of promotion at a time!';
+    END IF;
+    SET total_money = mod_order.total;
+    SET limit_point = total_money * 0.7 / 1000;
+    IF limit_point < redem_point THEN
+		SIGNAL SQLSTATE '01000'
+			SET MESSAGE_TEXT = 'Redemption promotion cannot greater than 70% of order total money!';
+	END IF;
+    
+    UPDATE PR_ORDER
+    SET PR_ORDER.promo_red = redem_point
+    WHERE PR_ORDER.order_id = mod_order_id;
+END $$
+DELIMITER ;
+
+
+# calculate cus promotion: 1 point = 1,000 VND
+DROP FUNCTION IF EXISTS func_cal_cus_promo;
+DELIMITER $$
+CREATE FUNCTION func_cal_cus_promo (
+	mem_id		INT,
+    redem_point	INT
+) RETURNS INT DETERMINISTIC
+BEGIN
+	DECLARE cus_point INT DEFAULT 0;
+    DECLARE promo_money INT DEFAULT 0;
+    SET cus_point = (SELECT acc_point FROM CUSTOMER WHERE mem_id = CUSTOMER.cus_id);
+    IF cus_point = NULL THEN
+		SIGNAL SQLSTATE '01000'
+			SET MESSAGE_TEXT = 'Cannot find cus!';
+            RETURN 0;
+	ELSE 
+		IF cus_point < redem_point THEN
+			SIGNAL SQLSTATE '01000'
+				SET MESSAGE_TEXT = 'Not enough point';
+				RETURN 0;
+		END IF;
+
+		SET promo_money = cus_point * 1000;
+    END IF;
+    UPDATE CUSTOMER
+    SET CUSTOMER.promo_point = CUSTOMER.promo_point - redem_point;
+    RETURN promo_money;
+END $$
+DELIMITER ;
+
+
+# trigger for creating new receipt when an ordern has been paid
+DROP TRIGGER IF EXISTS trig_create_receipt;
+DELIMITER $$
+CREATE TRIGGER trig_create_receipt AFTER UPDATE ON PR_ORDER 
+FOR EACH ROW
+BEGIN
+	IF PR_ORDER.stat = TRUE THEN
+		INSERT INTO RECEIPT VALUES(NEW.order_id, NEW.order_id, CURDATE(), CURTIME(), DEFAULT,
+									NEW.promo_red, NEW.br_id, NEW.cus_id, NEW.total);
+	END IF;
+END $$
+DELIMITER ;
+
+
+# trigger for update new total money after insert promotion redemption
+DROP TRIGGER IF EXISTS trig_update_total_money_ins;
+DELIMITER $$
+CREATE TRIGGER trig_update_total_money_ins AFTER UPDATE ON PR_ORDER
+FOR EACH ROW
+BEGIN
+	CALL proc_update_total_money(NEW.order_id);
+END$$
+DELIMITER ;
+
+# precedure for update new total money after updatepromotion redemption
+DROP PROCEDURE IF EXISTS proc_update_total_money;
+DELIMITER $$
+CREATE PROCEDURE proc_update_total_money(
+	mod_order_id	INT
+)
+BEGIN
+	DECLARE cus_id	INT DEFAULT 0;
+    DECLARE redem_point INT DEFAULT 0;
+    DECLARE promo_money INT DEFAULT 0;
+	SET cus_id = (SELECT cus_id FROM PR_ORDER WHERE mod_order_id = PR_ORDER.order_id);
+    SET redem_point = (SELECT promo_red FROM PR_ORDER WHERE mod_order_id = PR_ORDER.order_id);
+	SET promo_money = func_cal_cus_promo(cus_id, redem_point);
+    UPDATE PR_ORDER
+    SET PR_ORDER.total = PR_ORDER.total - promo_money
+    WHERE PR_ORDER.order_id = mod_order_id;
+END $$
+DELIMITER ;
